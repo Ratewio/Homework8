65c8e0fb5dbd04e2dad7a8580a3fac28c5193f9c
Ratewio <ratewio@mail.ru>
2026-02-25 13:07:18 +0500

    fix scopes

--- src/Main.java
+++ src/Main.java
@@ -13,32 +13,39 @@ public class Main {
             System.out.println("Матрица: ");
             printColors(colors);
             System.out.print("Введите угол поворота по часовой стрелке (90, 180, 270): ");
-            for (int turns = scanner.nextInt() / 90; turns != 0; --turns)
+            for (int turns = scanner.nextInt() / 90; turns != 0; --turns) {
                 colors = rotateColors(colors);
+            }
         }
     }
 
     public static void randomizeColors(int[][] colors) {
         Random random = new Random();
-        for (int i = 0; i != SIZE; ++i)
-            for (int j = 0; j != SIZE; ++j)
+        for (int i = 0; i != SIZE; ++i) {
+            for (int j = 0; j != SIZE; ++j) {
                 colors[i][j] = random.nextInt(256);
+            }
+        }
     }
 
     public static void printColors(int[][] colors) {
         for (int i = 0; i != SIZE; ++i) {
-            for (int j = 0; j != SIZE; ++j)
+            for (int j = 0; j != SIZE; ++j) {
                 System.out.format("%4d", colors[i][j]);
+            }
             System.out.println();
         }
         System.out.println();
     }
 
     public static int[][] rotateColors(int[][] colors) {
+        final int MOD_SIZE = SIZE - 1;
         int[][] rotatedColors = new int[SIZE][SIZE];
-        for (int i = 0; i != SIZE; ++i)
-            for (int j = 0; j != SIZE; ++j)
-                rotatedColors[j][SIZE - 1 - i] = colors[i][j];
+        for (int i = 0; i != SIZE; ++i) {
+            for (int j = 0; j != SIZE; ++j) {
+                rotatedColors[j][MOD_SIZE - i] = colors[i][j];
+            }
+        }
         return rotatedColors;
     }
 }
